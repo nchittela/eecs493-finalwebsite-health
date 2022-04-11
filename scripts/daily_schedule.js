2a576759ddfa6c236134403daede4130bb03df6b
henleycp <henleycp@umich.edu>
2022-04-11 17:43:31 -0400

    Daily Task Css Fixed

--- scripts/daily_schedule.js
+++ scripts/daily_schedule.js
@@ -64,28 +64,26 @@ function addButton() {
     // get task text
     const taskText = document.getElementById("addTaskForm")[0].value;
 
-    // create a new outer div element
-    outerNewDiv = document.createElement("div");
-    outerNewDiv.setAttribute("class", "fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event");
+    fcEvent = document.createElement("div");
+    fcEvent.setAttribute("class", "fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event");
 
     // crete a new inner div element
-    innerNewDiv = document.createElement("div");
-    innerNewDiv.setAttribute("class", "fc-event-main")
+    fcMain = document.createElement("div");
+    fcMain.setAttribute("class", "fc-event-main")
 
     // and give inner div some content
     const newContent = document.createTextNode(taskText);
 
     // add the text node to the newly created div
-    innerNewDiv.appendChild(newContent);
+    fcMain.appendChild(newContent);
 
     // append new inner div to outer div
-    outerNewDiv.appendChild(innerNewDiv);
-    outerNewDiv.id = taskText
+    fcEvent.appendChild(fcMain);
 
     // add the newly created element and its content into the DOM
     // if the top won't overrun the size
-    const currentDiv = document.getElementById("external-events");
-    currentDiv.appendChild(outerNewDiv)
+    const currentDiv = document.getElementById("addedEvents");
+    currentDiv.appendChild(fcEvent)
 
     // add task to task list dictionary with its category
     // 0 is nothing, 1 is Exercise, 2 is Productivity 3 is Mental Wellbeing
